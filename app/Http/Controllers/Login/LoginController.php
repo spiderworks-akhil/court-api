75c14ef6b70906236c2aeccfde158e830df4154b
Akhil Joy <b2akhilmj@gmail.com>
2021-04-30 15:49:22 +0530

    Documentation

--- app/Http/Controllers/Login/LoginController.php
+++ app/Http/Controllers/Login/LoginController.php
@@ -49,7 +49,54 @@ class LoginController extends Controller
     public function login(Request $request){
         // email and token needs to pass here
         try{
-            $user = file_get_contents('https://www.googleapis.com/oauth2/v3/userinfo?access_token='.$request->token);
+            // $user = file_get_contents('https://www.googleapis.com/oauth2/v3/userinfo?access_token='.$request->token);
+
+            $client = new \Google_Client(['client_id' => $request->client_id]);  // Specify the CLIENT_ID of the app that accesses the backend
+            $payload = $client->verifyIdToken($request->id_token);
+            if ($payload) {
+                $userid = $payload['sub'];
+
+                if(!empty($payload['email'])){
+
+                    $u = User::where('email',$payload['email'])->first();
+                    if(!$u){
+                        $u = new User();
+                        $u->is_google_login = 1;
+                        $u->google_token = $request->id_token;
+                        $u->name = $payload['name'];
+                        $u->email = $payload['email'];
+                        $u->password = Hash::make(rand(1111,9999).rand(1111,2222));
+                        if(strpos($payload['email'],'@spiderworks.in') !== false){
+                            $u->is_admin = 1;
+                        }
+                        $u->save();
+                    }
+
+                    $token = $u->createToken('user-token')->plainTextToken;
+
+                    $response = [
+                        'status' => true,
+                        'token' => $token,
+                        'user' => $u
+                    ];
+
+                }else{
+                    $response = [
+                        'status' => false,
+                        'token' => 'Invalid token, Failed to fetch user details'
+                    ];
+                }
+
+
+            } else {
+                dd($payload);
+                $response = [
+                    'status' => false,
+                    'token' => 'Invalid token, Failed to fetch user details'
+                ];
+            }
+
+
         }catch(\Exception $e){
             $response = [
                 'status' => false,
@@ -58,36 +105,8 @@ class LoginController extends Controller
             return response($response, 403);
         }
 
-        $user = json_decode($user);
-        if(!empty($user->email)){
 
-            $u = User::where('email',$user->email)->first();
-            if(!$u){
-                $u = new User();
-                $u->is_google_login = 1;
-                $u->google_token = $user->token;
-                $u->name = $user->name;
-                $u->email = $user->email;
-                $u->password = Hash::make(rand(1111,9999).$user->email.rand(1111,2222));
-                if(strpos($user->email,'@spiderworks.in') !== false){
-                    $u->is_admin = 1;
-                }
-                $u->save();
-            }
 
-            $token = $u->createToken('user-token')->plainTextToken;
-
-            $response = [
-                'status' => true,
-                'token' => $token
-            ];
-
-        }else{
-            $response = [
-                'status' => false,
-                'token' => 'Invalid token, Failed to fetch user details'
-            ];
-        }
         return response($response, 200);
     }
 }
